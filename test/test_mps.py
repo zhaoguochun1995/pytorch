6446ea993a310dcbbf438bc395b8aab9b3d5f938
arnaudstiegler <arnaud.stiegler@gmail.com>
2022-10-16 20:23:31 -0400

    op implemented, not tested

--- test/test_mps.py
+++ test/test_mps.py
@@ -4868,6 +4868,7 @@ class TestNLLLoss(TestCase):
 
         helper((2, 8, 4, 5), torch.exp)
         helper((2, 8, 3, 5), torch.exp2)
+        helper((2, 8, 3, 5), torch.expm1)
         helper((2, 8, 3, 5), torch.log)
         helper((2, 8, 3, 5), torch.cos)
 
